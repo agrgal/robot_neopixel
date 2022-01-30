8b5b87b9f7ccf16cd15997a0261723e59482e5d1
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2022-01-30 08:50:03 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    basic.pause(2000)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 88)
+    basic.pause(2000)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 88)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    basic.pause(2000)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    maqueen.motorStop(maqueen.Motors.All)
+})
+basic.forever(function () {
+	
+})
