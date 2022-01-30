4e8897a017d07b64f1fe03b3f648668efa86ccba
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2022-01-30 11:15:32 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    strip.clear()
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
@@ -6,15 +7,23 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 88)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    strip.show()
     basic.pause(2000)
+    strip.clear()
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    strip.show()
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 88)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
     basic.pause(2000)
+    strip.clear()
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     maqueen.motorStop(maqueen.Motors.All)
 })
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
 	
 })
